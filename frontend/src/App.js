5836fad38a923f6b74167601303e4224bb6adc4d
sumit <sumitkprasad123@gmail.com>
2023-05-06 08:57:32 +0530

    a

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,13 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
 
+
 function App() {
   return (
     <div className="App">
       {/* <Admin/> */}
       <Navbar />
+   
       <Home />
       <Footer />
     </div>
