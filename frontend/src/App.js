60f59343c5de21a46758c8c7dea361a409530fdb
Saurav Bhardwaj <bhardwajs124@gmail.com>
2023-05-07 23:00:40 +0530

    adding single product page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,20 +4,23 @@ import Home from "./pages/Home";
 import AllRoutes from './components/AllRoutes';
 import Footer from "./components/Footer";
 import Mens from './pages/mProduct/Mens';
+import SingleProductPage from "./pages/SingleProductPage";
+// import Admin from "./pages/Admin";
 
 
 
 function App() {
   return (
     <div className="App" onReset={true}>
-      <Admin/>
-      <Navbar />
-      <AllRoutes/>
-      <Home />
-
+      {/* <Admin /> */}
+      {/* <Navbar /> */}
+      {/* <AllRoutes/> */}
+      {/* <Home /> */}
+      {/* <Mens /> */}
      
+     <SingleProductPage />
 
-      <Footer />
+      {/* <Footer /> */}
 
     </div>
   );
